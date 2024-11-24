a7d5b790391cf16e69965eeeb004329404059277
tian <2469249038@qq.com>
2024-11-24 13:54:12 +0800

    重构了Test中的store_once_data

--- Test.py
+++ Test.py
@@ -1,20 +1,21 @@
 
-
-
 def store_once_data(func):
     data_dict={}
     def inner(data,site):
         try:
-            if data_dict[site]!=data:
-                data_dict[site]=data
-                func(data)
+            if data_dict[site]==data:
+                return
         except KeyError:
-            data_dict[site]=data
-            func(data)
+        	pass
+        data_dict[site]=data
+        func(data)
         #data_dict.setdefault(site)
     return inner
 
 print_difference= store_once_data(print)
 
-        
+
+
+
+
 
