4d910209b7481f56b7eef8f02c3d7fd6876eb975
School <Schoolnone>
2024-11-30 16:35:43 +0800

    将空格转为Tab

--- Test.py
+++ Test.py
@@ -1,16 +1,16 @@
 
 def store_once_data(func):
-    data_dict={}
-    def inner(data,site):
-        try:
-            if data_dict[site]==data:
-                return
-        except KeyError:
-        	pass
-        data_dict[site]=data
-        func(data)
-        #data_dict.setdefault(site)
-    return inner
+	data_dict={}
+	def inner(data,site):
+		try:
+			if data_dict[site]==data:
+				return
+		except KeyError:
+			pass
+		data_dict[site]=data
+		func(data)
+		#data_dict.setdefault(site)
+	return inner
 
 print_difference= store_once_data(print)
 
