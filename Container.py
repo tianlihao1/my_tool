4fbd31f5a603b88c039db381d81d1c9727c8b597
tian <2469249038@qq.com>
2024-12-08 00:53:07 +0800

    Container中的member属性改为members \n 新增MemberVisitor类，MemberVisitor作为Container.add的一种快捷方式，可以通过通过索引(名字)来访问，增加，修改Container.members对应的member。 \n Container和MemberVisitor支持in和迭代，每次迭代返回一个（name,member）。可以通过调用Container的get_MemberVisitor来获取该对象的MemberVisitor。

--- Container.py
+++ Container.py
@@ -1,13 +1,22 @@
-import pygame
+
 class Container():
 	def __init__(self,every_frame_function=None):
 		self.member_name_sort=[]
-		self.member={}
+		self.members={}
 		self.auto_name=0
 		self.bind(every_frame_function)
 		
+	def __contains__(self,item):
+		return item in self.members.keys()
+		
+	def keys(self):
+		return self.member_name_sort
+	
+	def values(self):
+		return self.members.values()
+	
 	def __getitem__(self,key):
-		return self.member[key]
+		return self.members[key]
 
 	def __setitem__(self,key,value):
 		auto_give_name=False
@@ -15,26 +24,29 @@ class Container():
 			auto_give_name=True
 		self.add(value,auto_give_name,key)
 
-	def add(self,object,name=None,auto_give_name=True):
+	def add(self,member,name=None,auto_give_name=True):
 #		if name!=None:
 #			self.member_name_sort.append(name)
 		
 		if (name is None) and auto_give_name:
 			name=self.auto_give_name()
-		
-		self.member_name_sort.append(name)
-		self.member[name]=object
-		object.father=self
-		object.name=name
+			
+		if not name in self:
+			member.father=self
+			member.name=name
+			self.member_name_sort.append(name)
+
+		self.members[name]=member
+
 
 		return name
 
 	def remove(self,name):
-		del self.member[name]
+		del self.members[name]
 		self.member_name_sort.remove(name)
 
 	def auto_give_name(self):
-		member_keys=self.member.keys()
+		member_keys=self.members.keys()
 		self.auto_name+=1
 		while True:
 			if not self.auto_name in member_keys:
@@ -42,39 +54,39 @@ class Container():
 			self.auto_name+=1
 
 	def change_start_status(self,status=True):
-		for item in self.member:
+		for item in self.members:
 			item.start=status
 
 	def change_visible_status(self,status=True):
-		for item in self.member:
+		for item in self.members:
 			item.visible=status
 
 	def change_event_enable_status(self,status=True):
-		for item in self.member:
+		for item in self.members:
 			item.event_enable=status
 
 	def __iter__(self):
-		return self.member.__iter__()
+		return ((name,member) for name,member in self.members.values())
 
 	def blit(self):
-		for name in self.member_name_sort[::-1]:
-			self.member[name].blit()
+		for name in reversed(self.member_name_sort):
+			self.members[name].blit()
 
 	def check(self,event):
 		#print(event)
-		for item in self.member.values():
+		for item in self.members.values():
 			item.check(event)
 			
 	def frame_update(self):
-		for item in self.member.values():
+		for item in self.members.values():
 			item.frame_update()
 		if self.every_frame_function:self.every_frame_function(self)
 
 	def get_members_name(self):
-		return self.member.keys()
+		return self.members.keys()
 
 	def get_members(self):
-		return self.member
+		return self.members
 
 #	def update(self,*args,**kargs):
 #		for i in self.member.values():
@@ -82,3 +94,52 @@ class Container():
 
 	def bind(self,func=None):
 		self.every_frame_function=func
+	
+	def get_MemberVisitor(self):
+		return MemberVisitor(self)
+
+
+class MemberVisitor():
+	def __init__(self,father:Container):
+		self.father=father
+	
+	def __getitem__(self,key):
+		return self.father[key]
+	
+	def __setitem__(self,key,value):
+		self.father.add(value,key)
+	
+	def __delitem__(self,key):
+		self.father.remove(key)
+		
+	def __len__(self):
+		return len(self.father.members)
+		
+	def __iter__(self):
+		return ((name,member) for name,member in self.father.members.items())
+		
+	def __contains__(self,item):
+		return item in self.father
+		
+	
+		
+
+if __name__=='__main__':
+	a=Container()
+	print(id(a))
+	a.add(Container(),1)
+	b=MemberVisitor(a)
+	for i in b:
+		print(i)
+	print(dict(b))
+#	b[1]=a
+#	c=Container()
+#	b[1]=c
+#	print(id(c))
+#	print(id(b.father.member[1]))
+#	print(b.father.member_name_sort)
+#	print(i for i in b)
+#	if 1 in b:
+#		print(1)
+
+
