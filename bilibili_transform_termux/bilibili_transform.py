a743665bb53933b2ac538662114dbfb257d3b61d
tian <2469249038@qq.com>
2025-01-31 15:41:27 +0800

    bilibili_transform_termux是一个在Termux中使用ffmpeg转码合成bilibili视频和提取bilibili视频的音频的工具

---
+++ bilibili_transform_termux/bilibili_transform.py
@@ -0,0 +1,177 @@
+#!/data/data/com.termux/files/usr/bin/python3
+import os
+import json
+import functools
+import argparse
+from collections import namedtuple
+
+with open('Setting.json','r') as file:
+	setting_map=json.load(file)
+Setting=namedtuple('Setting',setting_map.keys())
+
+Setting=Setting(**setting_map)
+
+#class Setting:
+#	top_dir='/storage/emulated/0/Android/data/tv.danmaku.bili/download/'
+
+#		#aim_audio_dir=
+#		aim_video_dir='/storage/emulated/0/1work/bilibili_transform/video'
+
+@functools.lru_cache
+def get_all_res_path(top_dir,audio=True):
+	dir_list=[]
+	for ch_dir in os.listdir(top_dir):
+		#print(ch_dir)
+		path,path0=get_res_path(os.path.join(top_dir,ch_dir))
+		#if audio:
+			#path=os.path.join(path,'audio.m4s')
+		#else:
+			#path=os.path.join(path,'video.m4s')
+		dir_list.append((path,path0))
+	return dir_list
+
+@functools.lru_cache
+def get_res_path(subdirectory):
+	
+	subdirectory=os.path.join(subdirectory,os.listdir(subdirectory)[0])
+	for i in os.listdir(subdirectory):
+		path=os.path.join(subdirectory,i)
+		if os.path.isdir(path):
+			res_path=path
+	return res_path,os.path.join(subdirectory,'entry.json')
+
+def load_json(path):
+	with open(path) as file:
+		return json.load(file)
+
+def is_aim_dir_exist():
+	if not os.path.isdir(Setting.aim_audio_dir):
+		os.mkdir(Setting.aim_audio_dir)
+	if not os.path.isdir(Setting.aim_video_dir):
+		os.mkdir(Setting.aim_video_dir)
+
+def transform(mode,subdirectory):
+	res_path,title_json_path=get_res_path(subdirectory)
+	title= get_title_from_json(title_json_path)
+	is_aim_dir_exist()
+		
+	audio_path=os.path.join(res_path,'audio.m4s')
+	if mode=='v':
+		video_path=os.path.join(res_path,'video.m4s')
+		os.system(f'ffmpeg -n -i {audio_path} -i {video_path} -c copy "{os.path.join(Setting.aim_video_dir,title)}.mp4"')
+		#print(f'ffmpeg -n -i {audio_path} -i {video_path} -c copy "{os.path.join(Setting.aim_video_dir,title)}.mp4"')
+	else:
+		os.system(f'ffmpeg -n -i {audio_path} "{os.path.join(Setting.aim_audio_dir,title)}.mp3"')
+		#print(f'ffmpeg -n -i {audio_path} "{os.path.join(Setting.aim_audio_dir,title)}.mp3"')
+
+	
+def get_title_from_json(title_json_path):
+	title=load_json(title_json_path)['title']
+	title=''.join(title.split())
+	return title
+
+
+
+def handle_mode(args):
+	if args.audio:
+		sign='a'
+	elif args.video:
+		sign='v'
+	else:
+		sign=args.mode
+	return sign
+
+def handle_list_parse(args):
+	if args.list:
+		print_info()
+		return True
+
+def handle_choose_parse(args):
+	if args.choose:
+		subdirectories=os.listdir(Setting.top_dir)
+		return (subdirectories[i]  for i in args.choose if i <= len(subdirectories)-1)
+
+def print_info():
+	for num,i in enumerate(get_all_res_path(Setting.top_dir)):
+		#print(len(get_all_res_path(Setting.top_dir)))
+		print(f'{num}: {get_title_from_json(i[1])}')
+		
+
+def set_parse():
+	#视频还是音频
+	parser=argparse.ArgumentParser()
+	group=parser.add_mutually_exclusive_group()
+	group.add_argument('-video','-v',action='store_true',help='输出为视频')
+	group.add_argument('-audio','-a',action='store_true',help='输出为音频')
+	group.add_argument('-mode','-m',help='选择模式： a：转为音频，v：转为视频',default='a',choices=['a','v'])
+	
+	#列出缓存视频及其序号
+	parser.add_argument('-list','-l',action='store_true',help='列出缓存视频及其序号')
+	#选择要处理的缓存的序号
+	parser.add_argument('-choose','-c',type=int,nargs='*',help='选择要处理的缓存的序号，只处理传入给该选项的缓存视频')
+	return parser
+
+def handle_parses(parser):
+	args=parser.parse_args()
+	
+	if handle_list_parse(args):
+		return 
+	
+	mode=handle_mode(args)
+	
+	choices=handle_choose_parse(args)
+	if not choices:
+		choices=os.listdir(Setting.top_dir)
+	for i in choices:
+		transform(mode,os.path.join(Setting.top_dir,i))
+	
+#print_info()
+
+def main():
+	handle_parses(set_parse())
+
+
+
+
+
+
+
+
+#def main(audio=True):
+#	for file_dir,title_path in get_all_res_path(Setting.top_dir):
+#		pass
+		
+			#print(f'ffmpeg -i {audio_path} "{os.path.join(aim_audio_dir,title)}.mp3"')
+#			print(aim_audio_dir)
+#			print(os.path.join(aim_audio_dir,title))
+		
+#print(get_path(top_dir))
+#print(os.listdir())
+
+
+#def a():
+#		audio=True
+#		file,title_path=('/storage/emulated/0/Android/data/tv.danmaku.bili/download/573151831/c_1190134131/16', '/storage/emulated/0/Android/data/tv.danmaku.bili/download/573151831/c_1190134131/entry.json')
+#		title=load_json(title_path)['title']
+#		audio_path=os.path.join(file,'audio.m4s')
+#		if not audio:
+#			video_path=os.path.join(file,'video.m4s')
+#			os.system(f'ffmpeg -i {audio_path} -i {video_path} -c copy {title}.mp4')
+#		else:
+#			
+			#os.system(f'ffmpeg -i {audio_path} {title}.mp3')
+			#print(f'ffmpeg -i {audio_path} {title}.mp3')
+#a()
+
+
+#main(len(sys.argv)==1)
+#is_aim_dir_exist()
+
+#for file,title_path in get_path(top_dir):
+#	print(file,'  ',title_path)
+if __name__=='__main__':
+	#print(get_all_res_path('/storage/emulated/0/Android/data/tv.danmaku.bili/download/'))
+	main()
+	#pass
+	#print(Setting)
+#transform('a','/storage/emulated/0/Android/data/tv.danmaku.bili/download/113825491714363/')
--- test/test_10step.py
+++ test/test_10step.py
@@ -1,7 +1,18 @@
 import itertools
+import random
+
 class Setting():
 	width=6
 	height=6
+	gene_group_length=2
+	start_num=100
+	chromosome_length=10
+	aim=10000
+	
+	exchange_rate=0.7
+	mutate_rate=0.1
+	realign_rate=0.1
+	remain_rate=0.8
 
 class GameMap():
 	def __init__(self,width,height):
@@ -15,12 +26,13 @@ class GameMap():
 		self.rotated_angel=0
 
 	def print_for_str(self):
-		for x in range(self.width):
-			for y in range(self.height):
-				print(self[(x,y)],end='  ')
+		for y in range(self.height):
+			for x in range(self.width):
+				print(self[(x,y)],end='\t')
 			print()
 			
 	def __getitem__(self,key):
+		#print(key)
 		return self.circles_dict[key]
 		
 	def get_total_angel(self):
@@ -29,6 +41,7 @@ class GameMap():
 	def action(self,x,y):
 		while True:
 			try:
+				#print(x,y)
 				f_x,f_y=self[(x,y)].turn()
 			except KeyError:return
 			else:
@@ -37,10 +50,23 @@ class GameMap():
 			y+=f_y
 			if x<0 or y<0:
 				return
-
+	def action_from(self,it,show=False):
+		#print(type(it[0]))
+		#print('action',it)
+		for i in it:
+			#print('action_from',it)
+			self.action(*i)
+			if show:
+				self.print_for_str()
+				print()
+	
+	def once_game_test(self,it):
+		self.reset()
+		self.action_from(it)
+		return self.get_total_angel()
 
 class Circle():
-	angel_to_forward={0:(0,1),90:(1,0),180:(0,-1),270:(-1,0)}
+	angel_to_forward={0:(0,-1),90:(1,0),180:(0,1),270:(-1,0)}
 	def __init__(self,x,y):
 		self.x,self.y=x,y
 		self.reset()
@@ -57,8 +83,198 @@ class Circle():
 		self.angel=next(self._gen_angel)
 		return self.angel_to_forward[self.angel]
 		
+		
+def split_part(it,step):
+	result=[]
+	num=0
+	for i in it:
+		num+=1
+		result.append(i)
+		if num==step:
+			yield result
+			result=[]
+			num=0
+	if result:
+		yield result
+
+
+
+class Evolution():
+	def __init__(self,chromosomes,exchange_rate,mutate_rate,realign_rate,remain_rate):
+		self.chromosomes=chromosomes
+		self.exchange_rate=exchange_rate
+		self.mutate_rate=mutate_rate
+		self.realign_rate=realign_rate
+		self.remain_rate=remain_rate
+
+	def gene_exchange(self,c0,c1):
+		newc0,newc1=[],[]
+		#print('c',c0,'\t',c1)
+		for g0,g1 in zip(c0,c1):
+			if random.random()<=self.exchange_rate:
+				newc0.extend(g1)
+				newc1.extend(g0)
+			else:
+				newc0.extend(g0)
+				newc1.extend(g1)
+		c0.rebuild(newc0)
+		c1.rebuild(newc1)
+		#print('c',c0,'\t',c1)
+		#print('new',newc0,'\t',newc1)
+		return newc0,newc1
+	
+	def gene_mutate(self,c):
+		result=[]
+		for g in c:
+			if random.random()<=self.mutate_rate:
+				#print(g)
+				site=random.randint(0,len(g)-1)
+				g[site]=c.get_random_gene_func()
+			result.extend(g)
+		c.rebuild(result)
+		return result
+				
+	
+	def gene_realign(self,c):
+		result=[]
+		for g in c:
+			if random.random()<=self.realign_rate:
+				random.shuffle(g)
+			result.extend(g)
+		c.rebuild(result)
+		return result
+	
+	def select(self,select_func):
+		
+		for c in self.chromosomes:
+			self.gene_mutate(c)
+			self.gene_realign(c)
+			
+		
+		random.shuffle(self.chromosomes)
+		chromosomes=self.chromosomes[:]
+		while True:
+			if len(chromosomes) <= 1:
+				break
+			c0=chromosomes.pop()
+			c1=chromosomes.pop()
+			self.gene_exchange(c0,c1)
+		
+		
+		self.chromosomes.sort(key=select_func,reverse=True)
+		num=int(self.remain_rate*len(self.chromosomes))
+		result=self.chromosomes[:num]
+		result.extend([i.build(i.gene_sequence) for i in self.chromosomes[:len(self.chromosomes)-num]])
+		#print(result)
+		self.chromosomes=result
+		
+		
+	def start_evolution(self,aim,select_func):
+		num=1
+		constant_going=True
+		while True:
+			try:
+				#print(self.chromosomes)
+				self.select(select_func)
+				#print(self.chromosomes)
+				print(f'第{num}代，最高分数{select_func(self.chromosomes[0])}，其序列为{self.chromosomes[0]}')
+				num+=1
+				if select_func(self.chromosomes[0])>=aim and constant_going:
+					raise ZeroDivisionError
+			except (ZeroDivisionError,KeyboardInterrupt):
+				#print(1,self.chromosomes[0].gene_sequence)
+				print(f'第{num}代，最高分数{select_func(self.chromosomes[0])}，其序列为{self.chromosomes[0]}')
+				sign=input('continue?(c/y/n)')
+				if sign=='n':break
+				elif sign=='c':
+					constant_going=False
+				
+				
+				
+			
+		
+
+
+class Chromosome():
+	def __init__(self,gene_sequence,gene_group_length,get_random_gene_func=None):
+		self.gene_sequence=gene_sequence
+		self.gene_group_length= gene_group_length
+		self.get_random_gene_func= get_random_gene_func
+	
+	def rebuild(self,it):
+		self.gene_sequence=list(it)
+		
+	def build(self,gene_sequence):
+		return Chromosome(gene_sequence,self.gene_group_length,self.get_random_gene_func)
+		
+	
+	def __iter__(self):
+		return split_part(self.gene_sequence,self.gene_group_length)
+	
+	def __str__(self):
+		string=''
+		for gene_group in self:
+			for gene_element in gene_group:
+				string+=str(gene_element)
+			string+=' '
+		return string
+	
+	def __len__(self):
+		return len(self.gene_sequence)
+	
+def get_random_gene():
+	return (random.randint(0,Setting.width),random.randint(0,Setting.height))
+	
+def get_many_chromosomes(num,chromosome_length,get_random_gene_func):
+	#result=[for for i in range(chromosome_length)]
+	result=[]
+	for i in range(num):
+		result.append(Chromosome([get_random_gene_func() for j in range(chromosome_length)],Setting.gene_group_length,get_random_gene_func))
+	return result
+			
+
+def select_func(func):
+	def inner(it):
+		return func(it.gene_sequence)
+	return inner
+
+def main():
+	print('main')
+	evolution= Evolution(get_many_chromosomes(Setting.start_num,Setting.chromosome_length,get_random_gene),Setting.exchange_rate,Setting.mutate_rate,Setting.realign_rate,Setting.remain_rate)
+	game=GameMap(Setting.height,Setting.width)
+	print('start')
+	evolution.start_evolution(Setting.aim,select_func(game.once_game_test))
+	#evolution.gene_exchange(*get_many_chromosomes(2,10,get_random_gene))
+	#print(select_func(game.once_game_test)(get_many_chromosomes(1,10,get_random_gene)[0]))
+#	a=get_many_chromosomes(3,10,get_random_gene)
+#	
+#	a.sort(key=select_func(game.once_game_test),reverse=True)
+#	for i in a:
+#		print(select_func(game.once_game_test)(i))
+
+
+
+		
 if __name__ =='__main__':
-	game=GameMap(6,6)
-	game.action(0,0)
-	game.action(0,0)
-	game.print_for_str()
+#	game=GameMap(6,6)
+#	game.action(0,0)
+#	game.action_from(((0,0),(0,0)),show=True)
+#	game.print_for_str()
+	#print(game.get_total_angel())
+#	print(list(zip(split_part((1,2,3,4,5),2) ,split_part((5,4,3,2,1),2))))
+	#e=Evolution()
+#	c0=Chromosome((1,2,3,4,5),2)
+#	game=GameMap(Setting.height,Setting.width)
+#	print( select_func(game.once_game_test)(get_many_chromosomes(1,10,get_random_gene)[0]))
+	main()
+	
+#	a=[(1, 1),(3, 1),(2, 2),(1, 4),(4, 4),(2, 3),(1, 2),(1, 2) ,(3, 2),(4, 0)]
+#	print(a)
+#	gamemap=GameMap(6,6)
+	#gamemap.action_from(a,show=True)
+	#print(gamemap.get_total_angel())
+
+	
+	#print([get_random_gene() for i in range(10)])
+	pass
+	
